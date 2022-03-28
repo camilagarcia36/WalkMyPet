4940588ab6f8bf5335bcf097e17e1b9e7545665e
camilagarcia36 <garciiaperezz@gmail.com>
2022-03-28 22:24:41 +0200

    Vistas 005 y 006 añadidas al repositorio

---
+++ app/src/main/java/com/example/walkmypet/CuentaActivity.java
@@ -0,0 +1,16 @@
+package com.example.walkmypet;
+
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CuentaActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cuenta_dueno);
+    }
+}
---
+++ app/src/main/java/com/example/walkmypet/petlistAdapter.java
@@ -0,0 +1,16 @@
+package com.example.walkmypet;
+
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class petlistAdapter extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.petlist_format);
+    }
+}
